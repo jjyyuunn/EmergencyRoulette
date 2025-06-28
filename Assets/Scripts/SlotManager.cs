9637bffe65b693baa3266c7f9c5f50bf34801ad1
ryudamin <129927144+ryudamin@users.noreply.github.com>
2025-06-28 16:16:15 +0900

    Visualize Slots

--- Assets/Scripts/SlotManager.cs
+++ Assets/Scripts/SlotManager.cs
@@ -1,32 +1,64 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace EmergencyRoulette
 {
     public class SlotManager : MonoBehaviour
     {
+        [SerializeField] private GameObject slotPrefab;
+        [SerializeField] private Transform slotParent;
+
+        private SlotColumnScroller[,] slotInstances;
+
         private SymbolLibrary _symbolLibrary;
         private SymbolPicker _symbolPicker;
         private SlotBoard _slotBoard;
         
-        public SlotColumnScroller Slot0;
-
         public void Start()
         {
             _symbolLibrary = new SymbolLibrary();
             _symbolPicker = new SymbolPicker(_symbolLibrary);
             _slotBoard = new SlotBoard(_symbolPicker);
+
+            CreateSlotVisuals(_slotBoard.RowCount, _slotBoard.ColumnCount);
             PrintBoard();
         }
-        
+
+        public void CreateSlotVisuals(int row, int column)
+        {
+            slotInstances = new SlotColumnScroller[column, row];
+
+            for (int x = 0; x < column; x++)
+            {
+                for (int y = 0; y < row; y++)
+                {
+                    GameObject slotObj = Instantiate(slotPrefab, slotParent);
+                    var scroller = slotObj.GetComponent<SlotColumnScroller>();
+                    slotInstances[x, y] = scroller;
+
+                    RectTransform rt = slotObj.GetComponent<RectTransform>();
+                    rt.anchoredPosition = new Vector2(x * 120f, -y * 120f);
+                }
+            }
+        }
+
         public void StartSpin()
         {
             _slotBoard.Spin();
-            StartCoroutine(Slot0.StartSpin(_slotBoard.Get(0,0)));
-            
+
+            for (int x = 0; x < _slotBoard.ColumnCount; x++)
+            {
+                for (int y = 0; y < _slotBoard.RowCount; y++)
+                {
+                    SymbolType symbol = _slotBoard.Get(x, y);
+                    slotInstances[x, y].StartCoroutine(slotInstances[x, y].StartSpin(symbol));
+                }
+            }
+
             PrintBoard();
-            
             AddResource();
         }
+
 
         // 플레이어 리소스 더하기
         private void AddResource()
